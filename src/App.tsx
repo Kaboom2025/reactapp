2095b0ce8b2f9d09d3e1dc36de1130b9b1084137
Kaboom2025 <redcar2025@gmail.com>
2023-12-30 18:03:41 -0800

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  const foods = ["Apple", "Orange", "Lelon", "melon"];
-  const handleSelectItem = (food: string) => {
-    console.log(food);
-  };
+  /// const foods = ["Apple", "Orange", "Lelon", "melon"];
+  ///const handleSelectItem = (food: string) => {
+  ///  console.log(food);
+  ///};
   return (
     <div>
-      <ListGroup
+      <Button onClick={() => console.log("clicked")}> yo</Button>
+
+      {/*<ListGroup
         foods={foods}
         heading={"Foods"}
         onSelectItem={handleSelectItem}
-      />
+  />*/}
     </div>
   );
 }
